23092ffb21b21ed2940ce9a14cdbc421ddbc7adf
Myat Thu Ko <nymyatthuko@gmail.com>
2020-05-28 22:18:57 -0400

    Completed project

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Myat Thu Ko on 5/28/20.
+//  Copyright © 2020 Myat Thu Ko. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let mission: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(mission) { mission in
+                NavigationLink(destination: MissionVIew(mission: mission, astronauts: self.astronauts)) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+        .navigationBarTitle("Moonshot")
+        }
     }
 }
 
